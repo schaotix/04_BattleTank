3ab729ae45b7acc8a5f7718610c344d75fd4ce01
schaotix <rane29@gmail.com>
2022-03-08 14:52:11 -0500

    3/8/22

---
+++ BattleTank/Source/BattleTank/OpenDoor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	InitialYaw = CurrentYaw;
+	TargetYaw += InitialYaw;
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor(DeltaTime);
+	}
+
+}
+
+
+
+void UOpenDoor::OpenDoor(float DeltaTime)
+{
+	
+}
+
